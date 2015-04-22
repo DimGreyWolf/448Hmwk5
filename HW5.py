c6428fbb4afb470be145d023c0ce19acdfaa6aa1
cnhenry <cnhenry@live.com>
2015-04-21 21:05:51 -0500

    Implemented basic GUI functionality

--- HW5.py
+++ HW5.py
@@ -4,11 +4,17 @@ from drug_database import DrugDatabase
 
 # This class is the view class of MVC architecture
 # and handles all GUI functionality
-class DrugAnalysis:
+class DrugAnalysis(QtGui.QWidget):
 
     def __init__(self):
         print('DrugAnalysis is now running')
+        super(DrugAnalysis,self).__init__()
+        self.initUI()
 
+    def initUI(self):
+        self.setGeometry(400,400,250,300)
+        self.setWindowTitle('Drug Analysis Program')
+        self.show()
 
 # This block of the code will execute the program
 # when python runs this specific script
--- drug_database.py
+++ drug_database.py
@@ -3,9 +3,12 @@ import os
 class DrugDatabase:
     # Constructor
     def __init__(self):
+        print('DrugDatabase initiated')
 
     # Reads the potential candidate drugs file
     def readCandidates(self,filename):
+        print('Reading candidate files')
 
     # Reads the example ideal drugs file
     def readExamples(self,filename):
+        print('Reading example files')
