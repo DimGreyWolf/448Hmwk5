0f3f11a895015f510a1daa271104110a78e6cf49
cnhenry <h724n828@gmail.com>
2015-04-22 07:05:22 -0500

    Added unit test for DrugDatabase class

--- test_database.py
+++ test_database.py
@@ -2,3 +2,9 @@ from drug_database import DrugDatabase
 
 test_class = DrugDatabase()
 test_class.readCandidates('hw5db1.txt') 
+
+for drug in range(0,43347):
+    text = ''
+    for element in range(0,16):
+        text += str(test_class.getCandidateData(drug,element)) + '|'
+    print(text)
