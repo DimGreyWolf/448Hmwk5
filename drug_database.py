380d62a243b07b3b0208f75d823ab86aabb88b91
cnhenry <cnhenry@live.com>
2015-04-21 20:34:09 -0500

    Began initial layout for DrugDatabase model class

---
+++ drug_database.py
@@ -0,0 +1,11 @@
+import os
+
+class DrugDatabase:
+    # Constructor
+    def __init__(self):
+
+    # Reads the potential candidate drugs file
+    def readCandidates(self,filename):
+
+    # Reads the example ideal drugs file
+    def readExamples(self,filename):
