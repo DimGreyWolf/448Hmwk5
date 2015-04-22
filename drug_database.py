685b07df0db490fa7eba92ddce8fe373f0c655fd
cnhenry <h724n828@gmail.com>
2015-04-22 10:23:17 -0500

    Added required member variables and related functions in DrugDatabase class

--- drug_database.py
+++ drug_database.py
@@ -9,6 +9,12 @@ class DrugDatabase:
         print('DrugDatabase initiated')
         self.candidates = []
         self.examples = []
+        self.ratio = 0.0
+        self.listDisplay = []
+        self.listNaa = []
+        self.listNan = []
+        self.listNna = []
+        self.listNnn = []
 
     # Reads the potential candidate drugs file
     def readCandidates(self,filename):
@@ -37,8 +43,45 @@ class DrugDatabase:
         return selectedElement
 
     # Obtains specific element of a specific example drug
+    # e.g getExampleData(5,0), gets the 0th element of the 5th example
     def getExampleData(self,drug,element):
         selectedExample = self.examples[drug]
         categories = selectedExample.split()
         selectedElement = categories[element]
         return selectedElement
+    
+    # Getter & Setter the ratio of the selected drug
+    def getRatio(self)
+        return self.ratio
+
+    def setRatio(self,ratio)
+        self.ratio = ratio
+
+    # Selects drug list to be displayed in list widget
+    def selectNaa(self):
+        self.listDisplay = self.listNaa
+
+    def selectNan(self):
+        self.listDisplay = self.listNan
+
+    def selectNna(self):
+        self.listDisplay = self.listNna
+
+    def selectNnn(self):
+        self.listDisplay = self.listNnn
+
+    # Updates drug list
+    def updateNaa(self,Naa)
+        self.listNaa = Naa
+
+    def updateNan(self,Nan)
+        self.listNan = Nan
+
+    def updateNna(self,Nna)
+        self.listNna = Nna
+
+    def updateNnn(self,Nnn)
+
+    # Returns display list
+    def getDisplayList(self)
+        return self.listDisplay
