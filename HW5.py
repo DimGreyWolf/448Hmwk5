921837aa73891025a644401fcd036f42ec5f0df8
cnhenry <h724n828@gmail.com>
2015-04-22 08:56:18 -0500

    Refactored GUI design to fit our requirements

--- HW5.py
+++ HW5.py
@@ -1,20 +1,35 @@
 import sys
 from PyQt4 import QtGui, QtCore
 from drug_database import DrugDatabase
+import pyqtgraph as pg
+import numpy as np
 
 # This class is the view class of MVC architecture
 # and handles all GUI functionality
-class DrugGraph(QtGui.QWidget):
+class DrugGraph:
 
     def __init__(self):
         print('DrugGraph is now running')
-        super(DrugGraph,self).__init__()
+        self.model = DrugDatabase()
         self.initUI()
+        #super(DrugGraph,self).__init__()
 
     def initUI(self):
-        self.setGeometry(400,400,250,300)
-        self.setWindowTitle('Drug Analysis Program')
-        self.show()
+        self.win = QtGui.QMainWindow()
+        self.win.resize(800,350)
+        self.win.setWindowTitle('Drug Analysis')
+        self.plot = pg.PlotWidget()
+        self.win.setCentralWidget(self.plot)
+        self.win.show()
+
+        self.selectWin = QtGui.QWidget()
+        self.selectWin.resize(300,150)
+        self.selectWin.setWindowTitle('Selected Drug')
+        self.selectWin.show()
+
+    def windowClicked(self):
+        print('Clicked')
+        drug, prompt = QtGui.QInputDialog.getText(win,'Selected Drug','Which drug?')
 
 # This block of the code will execute the program
 # when python runs this specific script
@@ -22,6 +37,7 @@ class DrugGraph(QtGui.QWidget):
 def main():
     app = QtGui.QApplication(sys.argv)
     drugGraph = DrugGraph()
+    #win.setCentralWidget(drugGraph)
 
     sys.exit(app.exec_())
 
