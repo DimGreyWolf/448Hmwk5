aa2c1cb14dceeec8b2b590afce6b34172286eac1
cnhenry <h724n828@gmail.com>
2015-04-21 22:42:44 -0500

    Added database file reading and get functions

--- drug_database.py
+++ drug_database.py
@@ -1,14 +1,37 @@
 import os
 
 class DrugDatabase:
+
     # Constructor
     def __init__(self):
         print('DrugDatabase initiated')
+        self.candidates = []
+        self.examples = []
 
     # Reads the potential candidate drugs file
     def readCandidates(self,filename):
         print('Reading candidate files')
+        candidatesFile = open(filename, 'rb')
+        for line in candidatesFile:
+	    elements = line.split()
+            self.candidates += elements
 
     # Reads the example ideal drugs file
     def readExamples(self,filename):
         print('Reading example files')
+        self.examples = open(filename, 'rb')
+        examplesFile = open(filename, 'rb')
+        for line in candidatesFile:
+            elements = line.split()
+            self.examples += elements
+
+    def getCandidateData(self,drug,element):
+        selectedDrug = self.candidates[drug]
+        selectedElement = selectedDrug[element]
+        return selectedElement
+
+    def getExamplesData(self,drug,element):
+        selectedExamples = self.examples[drug]
+        selectedElement = selectedExamples[element]
+        return selectedElement
+            
---
+++ test_database.py
@@ -0,0 +1,4 @@
+from drug_database import DrugDatabase
+
+test_class = DrugDatabase()
+test_class.readCandidates('hw5db1.txt') 
