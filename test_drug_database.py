d4dda1c38ec648fb98f195b5901301697ab36276
cnhenry <cnhenry@live.com>
2015-04-23 11:20:34 -0500

    Added class diagram relations

--- test_drug_database.py
+++ test_drug_database.py
@@ -1,8 +1,15 @@
 from drug_database import DrugDatabase
 
+# Initializes a test DrugDatabase class
 test_class = DrugDatabase()
+
+# Reads both candidate and example files in
 test_class.readCandidates('hw5db1.txt') 
 test_class.readExamples('hw5db2.txt')
+
+# Print output for both files
+
+print('Reading and formatting candidate data')
 
 for drug in range(0,43347):
     text = ''
@@ -10,8 +17,12 @@ for drug in range(0,43347):
         text += str(test_class.getCandidateData(drug,element)) + '|'
     print(text)
 
+print('Reading and formatting example data')
+
 for example in range(0,8):
     text = ''
     for element in range(0,8):
         text += str(test_class.getExampleData(example,element)) + '|'
     print(text)
+
+print('If displayed data matches data files, then test passes')
